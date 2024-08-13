1fc475f8c895ecc342f41f624df972ee39b725ae
alimu <alimuhamad.safa@gmail.com>
2024-08-12 21:22:34 -0400

    Completing activity

--- src/test/java/playground/Activity1.java
+++ src/test/java/playground/Activity1.java
@@ -1,21 +1,30 @@
 package playground;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Activity1 {
     //Write to navigate to facebook.com and print the Title.
-    ChromeDriver driver = new ChromeDriver();
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setupTest() {
+        driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+    }
 
     @Test
-    public void openChromeBrowser(){
-        driver.get("https://www.facebook.com/");
-        driver.getTitle();
-
+    public void testFacebookTitle() {
+        String applicationTitle = driver.getTitle();
+        Assert.assertEquals(applicationTitle, "xFacebook - log in or sign up");
     }
-    @BeforeMethod
-    public void BeforeMethod(){
-        System.out.println("I am hoping getting ahead of class");
+
+    @AfterMethod
+    public void cleanupTest() {
+        driver.quit();
     }
 }
